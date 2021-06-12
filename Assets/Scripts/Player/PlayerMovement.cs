730c87b388bef025f7beadb19e8472d4c2b31297
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2021-06-12 13:46:00 -0700

    Created PlayerMovement script

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private bool flipLeft = false;
+
+    void Start()
+    {
+        transform.position = new Vector3(-4, transform.position.y, transform.position.z);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            transform.GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
