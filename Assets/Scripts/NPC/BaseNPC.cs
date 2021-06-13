7eb85315cb4c495805177e2803537be73965ee09
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2021-06-12 21:47:59 -0700

    Add NPC scripts

---
+++ Assets/Scripts/NPC/BaseNPC.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Create an NPC
+ * Properties
+ * - Name
+ * - Instrument
+ * - Dialogue list
+ */
+
+public enum Instruments
+{
+    Trumpet,
+    Piano,
+    Bass,
+    Drums,
+    Guitar
+}
+
+public class BaseNPC:MonoBehaviour
+{
+    [TextArea]
+    public string name;
+    public Instruments instrument;
+    public List<string> dialogue;
+    public Font font;
+    private bool bandMember = false;
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public List<string> GetDialogue()
+    {
+        return dialogue;
+    }
+
+    public Font GetFont()
+    {
+        return font;
+    }
+
+    public bool GetMember()
+    {
+        return bandMember;
+    }
+}
