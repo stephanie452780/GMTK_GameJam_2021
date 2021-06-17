e5c54cd62e50935e7220f5324de6789f1dea972d
Jocelyn Zaman <44142922+jocelynzaman@users.noreply.github.com>
2021-06-16 19:43:28 -0700

    Create PageTransition script
    Navigate title screen pages
    - main page
    - controls page
    - credits page

---
+++ Assets/PageTransition.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PageTransition : MonoBehaviour
+{
+    public GameObject mainPage;
+    public GameObject controlsPage;
+    public GameObject creditsPage;
+
+    void Start()
+    {
+        mainPage.SetActive(true);
+        controlsPage.SetActive(false);
+        creditsPage.SetActive(false);
+    }
+
+    public void ShowControls()
+    {
+        mainPage.SetActive(false);
+        controlsPage.SetActive(true);
+        creditsPage.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        mainPage.SetActive(false);
+        controlsPage.SetActive(false);
+        creditsPage.SetActive(true);
+    }
+
+    public void ClickBack()
+    {
+        mainPage.SetActive(true);
+        controlsPage.SetActive(false);
+        creditsPage.SetActive(false);
+    }
+}
